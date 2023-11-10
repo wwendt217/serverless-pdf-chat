f3557bbec0f426c26f40afd7a973f5548f6373fb
wwendt217 <82593710+wwendt217@users.noreply.github.com>
2023-11-10 03:58:35 -0600

    Update main.py

--- backend/src/generate_response/main.py
+++ backend/src/generate_response/main.py
@@ -39,7 +39,7 @@ def lambda_handler(event, context):
         client=bedrock_runtime,
         region_name="us-east-1",
     ), Bedrock(
-        model_id="anthropic.claude-v2", client=bedrock_runtime, region_name="us-east-1"
+        model_id="ai21.j2-ultra-v1", client=bedrock_runtime, region_name="us-east-1"
     )
     faiss_index = FAISS.load_local("/tmp", embeddings)
 
